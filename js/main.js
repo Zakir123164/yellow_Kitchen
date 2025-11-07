fb7e6653903503358404585aeba598b1083374b3
Zakir123164 <jkrhossain8@gmail.com>
2025-11-07 09:25:32 +0600

    'Kitchen'

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// ---------------------------- slick slider -------------------------
+$('.restaurantlItem').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows:true,
+  prevArrow:'<i class="fa-solid fa-arrow-right"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right"></i>'
+ })
+
+
+//  $('.specialitieslItem').slick({
+//   slidesToShow: 7,
+//   slidesToScroll: 1,
+//   arrows:true,
+//   prevArrow:'<i class="fa-solid fa-arrow-right"></i>',
+//   nextArrow: '<i class="fa-solid fa-arrow-right"></i>'
+//  })
+
+
