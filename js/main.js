92be86bcd366b9e642c9bc23ef4616b7618af75f
Zakir123164 <jkrhossain8@gmail.com>
2025-11-14 02:12:52 +0600

    'update'

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
-// ---------------------------- slick slider -------------------------
-$('.restaurantlItem').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  arrows:true,
-  prevArrow:'<i class="fa-solid fa-arrow-right"></i>',
-  nextArrow: '<i class="fa-solid fa-arrow-right"></i>'
- })
-
-
-//  $('.specialitieslItem').slick({
-//   slidesToShow: 7,
+// // ---------------------------- slick slider -------------------------
+// $('.restaurantlItem').slick({
+//   slidesToShow: 4,
 //   slidesToScroll: 1,
 //   arrows:true,
-//   prevArrow:'<i class="fa-solid fa-arrow-right"></i>',
+//   prevArrow:'<i class="fa-solid fa-arrow-left"></i>',
 //   nextArrow: '<i class="fa-solid fa-arrow-right"></i>'
 //  })
 
 
+// ---------------------------- Slick Slider -------------------------
+$('.restaurantlItem').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows:false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  speed: 800,
+  prevArrow: '<i class="fa-solid fa-arrow-left prr"></i>',
+  nextArrow: '<i class="fa-solid fa-arrow-right nxt"></i>',
+
+  responsive: [
+    {
+      breakpoint: 1200, // laptops
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992, // tablets
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768, // mobile landscape
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false, // optional: hide arrows on small screens
+        dots:false // optional: add dots for better mobile UX
+      }
+    }
+  ]
+});
